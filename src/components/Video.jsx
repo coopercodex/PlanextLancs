09d10acc6600cc1fb2d3c3d111ff024262453a0e
Derek Cooper <derekcooper20@gmail.com>
2023-02-03 21:18:53 -0700

    Video fix

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Video.css'
 import marsVideo from '../assests/vecteezy_3-d-animated-footage-of-planet-mars_2158564.mp4'
 import { Link } from 'react-router-dom'
 
 
 export const Video = () => {
+  const videoRef = useRef();
+
+  const setPlayBack = () => {
+    videoRef.current.playbackRate = 0.6;
+  }
+
   return (
     <div className='hero'>
-      <video autoPlay loop muted id='video'>
+      <video autoPlay loop muted ref={videoRef} onCanPlay={setPlayBack} id='video'>
         <source src={marsVideo} type='video/mp4' />
       </video>
       <div className='content'>
