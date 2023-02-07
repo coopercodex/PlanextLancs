f0ff052814e57fe9cc25f559ccf0b72f985c8f05
Derek Cooper <derekcooper20@gmail.com>
2023-02-06 18:56:44 -0700

    Adds modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { Pricing } from "./routes/Pricing";
 import { Home } from "./routes/Home";
 import { Training } from "./routes/Training";
+import { Contact } from "./routes/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/training" element={<Training />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import astronaut from '../assests/vecteezy_apollo-11-mission-image-astronaut-edwin-aldrin-poses__107.jpeg'
+import "./Modal.css"
+export const Modal = ({open, onClose}) => {
+  if (!open) {
+    return null;
+  }
+
+  return (
+    <div onClick={onClose} className='overlays'>
+     <div onClick={(e) => {e.stopPropagation()}} className="modalContainer">
+      <img src={astronaut} alt="astronaut ad" />
+      <div className="modalRight">
+        <p className="closeBtn" onClick={onClose}>X</p>
+        <div className="contents">
+          <p>Want To Be The First?</p>
+          <h1>1 Basic Package</h1>
+          <p>Enter To Win!</p>
+        </div>
+        <div className="btnContainer">
+          <button className='btn-primary'>
+            <span>Yes!</span>, Take Me There!
+          </button>
+          <button className='btn-outline' onClick={onClose}>
+            <span className='bold'>No</span>, Thanks
+          </button>
+        </div>
+      </div>
+     </div>
+    </div>
+  )
+}
